b9bd1abeb5550ff3e6ef67c04721f446e1588f2d
Jeferson <jeferson.s.s@live.com>
2023-04-18 21:45:21 -0300

    Finalizado a pesquisa

--- src/Tela.js
+++ src/Tela.js
@@ -1,29 +1,79 @@
+import axios from "axios"
+import { useState } from "react";
+
+
 
 export default function Tela()
 {
-   return(
 
-   <div>
+    const [pokemon, setPokemon] = useState(null);
+    const [pesquisa, setPesquisa] = useState("");
+    const [alertErro, setAlertErro] = useState(false);
+    
 
-      <div className="input-group mb-3">
-         <input type="text" className="form-control" placeholder="Pesquise seu Pokemon" aria-label="Recipient's username" aria-describedby="button-addon2" />
-         <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
-      </div>
+    async function carregaPokemon()
+    {
+        try {
+            setPokemon(null)
+            setAlertErro(0)
+            const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon/"+pesquisa);
+            // Forma alternativa: .get(`https://pokeapi.co/api/v2/pokemon/${pesquisa}`)
+            setPokemon(data);
+        } catch (erro)
+        {
+            setAlertErro(true)
+        }
+    }
 
-      <div className="card">
+    function pokeNome(evento)
+    {
+        setPesquisa(evento.target.value)
+    }
 
-         <div className="card-header">
-            Nome do Pokemon
-         </div>
+    let cardPokemon = "";
+    
+    if (pokemon != null)
+    {
+        cardPokemon = <div className="card">
+            <div className="card-header">
+                { pokemon.name }
+            </div>
+            <img src={ pokemon.sprites.other["official-artwork"].front_default } className="card-img-top"  />
+            <div className="card-body">
+                <p>Altura: <strong> { pokemon.height} </strong></p>
+                <p>Peso: <strong> {pokemon.weight} </strong></p>
+                <div>
+                    Habilidades:
+                    <ul>
+                        {
+                        pokemon.abilities.map(function(item)
+                        {
+                            return (<li key={item.ability.name}>{ item.ability.name }</li>)
+                        })   
+                        }
+                        
+                    </ul>
 
-         <img src="https://placehold.co/400x200" className="card-img-top" alt="..." />
+                </div>
+            </div>
+        </div>;
+    }
 
-         <div className="card-body">
-            Detalhes
-         </div>
+    return(
 
-      </div>
-   </div>
+    <div>
+        <div className="input-group mb-3">
+            <input type="text" onChange={pokeNome} className="form-control" placeholder="Digite o nome" />
+            <button disabled={ pesquisa == "" } onClick={ carregaPokemon } className="btn btn-outline-secondary" type="button" >Pesquisar</button>
+        </div>
 
-  )
+        {/* Exibe o pokemon caso tenha sido feita uma pesquisa */}
+        { cardPokemon }
+        { (alertErro == true)? <div className="alert alert-danger" role="alert">
+                        Bichinho não encontrado!
+                    </div> : ""
+        }
+        
+    </div>
+    )
 }
